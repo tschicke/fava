c9ebb6f2bcbbaadc86e06e57fc75b67f999a447f
Tyler Schicke <tschicke@gmail.com>
2023-08-05 14:38:34 -0700

    Improve extension endpoint handling, and add apis
    This moves the endpoint handling to the FavaExtensionBase class, and
    adds the extension_endpoint decorator to allow extensions to register
    endpoints more easily.
    
    It also adds the ExtensionContext and class which is passed to all
    extension module functions. The context currently only contains an
    ExtensionApi, which allows the extension to call it's endpoints more
    easily.

--- frontend/src/extensions.ts
+++ frontend/src/extensions.ts
@@ -9,23 +9,101 @@ import { getUrlPath, urlFor } from "./helpers";
 import { log_error } from "./log";
 import { extensions } from "./stores";
 
+class ExtensionApi {
+  extension_name: string;
+
+  constructor(extension_name: string) {
+    this.extension_name = extension_name;
+  }
+
+  request(
+    endpoint: string,
+    method: string,
+    params: Record<string, string | number> | undefined,
+    body: object | undefined,
+  ) {
+    const url = urlFor(
+      `extension/${this.extension_name}/${endpoint}`,
+      params,
+      false,
+    );
+    let opts = {};
+    if (body) {
+      opts =
+        body instanceof FormData
+          ? { body }
+          : {
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(body),
+            };
+    }
+    return fetch(url, { method, ...opts }).then((res) => res.json());
+  }
+
+  get(endpoint: string, params: Record<string, string | number>) {
+    return this.request(endpoint, "GET", params, undefined);
+  }
+
+  put(endpoint: string, body: object | undefined) {
+    return this.request(endpoint, "PUT", undefined, body);
+  }
+
+  post(endpoint: string, body: object | undefined) {
+    return this.request(endpoint, "POST", undefined, body);
+  }
+
+  delete(endpoint: string, body: object | undefined) {
+    return this.request(endpoint, "DELETE", undefined, body);
+  }
+}
+
+export interface ExtensionContext {
+  api: ExtensionApi;
+}
+
 /**
  * The Javascript code of a Fava extension should export an object of this type.
  */
 export interface ExtensionModule {
   /** Initialise this Javascript module / run some code on the initial load. */
-  init?: () => void;
+  init?: (c: ExtensionContext) => void;
   /** Run some code after any Fava page has loaded. */
-  onPageLoad?: () => void;
+  onPageLoad?: (c: ExtensionContext) => void;
   /** Run some code after the page for this extension has loaded. */
-  onExtensionPageLoad?: () => void;
+  onExtensionPageLoad?: (c: ExtensionContext) => void;
 }
 
-async function loadExtensionModule(name: string): Promise<ExtensionModule> {
+export class ExtensionData implements Required<ExtensionModule> {
+  context: ExtensionContext;
+
+  extension: ExtensionModule;
+
+  constructor(context: ExtensionContext, extension: ExtensionModule) {
+    this.context = context;
+    this.extension = extension;
+  }
+
+  init(): void {
+    this.extension.init?.(this.context);
+  }
+
+  onPageLoad(): void {
+    this.extension.onPageLoad?.(this.context);
+  }
+
+  onExtensionPageLoad(): void {
+    this.extension.onPageLoad?.(this.context);
+  }
+}
+
+async function loadExtensionModule(name: string): Promise<ExtensionData> {
   const url = urlFor(`extension_js_module/${name}.js`, undefined, false);
   const mod = await (import(url) as Promise<{ default?: ExtensionModule }>);
   if (typeof mod.default === "object") {
-    return mod.default;
+    const context: ExtensionContext = {
+      api: new ExtensionApi(name),
+    };
+    return new ExtensionData(context, mod.default);
   }
   throw new Error(
     `Error importing module for extension ${name}: module must export "default" object`,
@@ -33,17 +111,17 @@ async function loadExtensionModule(name: string): Promise<ExtensionModule> {
 }
 
 /** A map of all extensions modules that have been (requested to be) loaded already. */
-const loaded_extensions = new Map<string, Promise<ExtensionModule>>();
+const loaded_extensions = new Map<string, Promise<ExtensionData>>();
 
 /** Get the extensions module - if it has not been imported yet, initialise it. */
-async function getExt(name: string): Promise<ExtensionModule> {
+async function getExt(name: string): Promise<ExtensionData> {
   const loaded_ext = loaded_extensions.get(name);
   if (loaded_ext) {
     return loaded_ext;
   }
   const ext = loadExtensionModule(name);
   loaded_extensions.set(name, ext);
-  (await ext).init?.();
+  (await ext).init();
   return ext;
 }
 
@@ -55,7 +133,9 @@ export function handleExtensionPageLoad(): void {
   for (const { name } of exts) {
     // Run the onPageLoad handler for all pages.
     getExt(name)
-      .then((m) => m.onPageLoad?.())
+      .then((m) => {
+        m.onPageLoad();
+      })
       .catch(log_error);
   }
   const path = getUrlPath(window.location);
@@ -63,7 +143,9 @@ export function handleExtensionPageLoad(): void {
     for (const { name } of exts) {
       if (path.startsWith(`extension/${name}`)) {
         getExt(name)
-          .then((m) => m.onExtensionPageLoad?.())
+          .then((m) => {
+            m.onExtensionPageLoad();
+          })
           .catch(log_error);
       }
     }
--- src/fava/ext/__init__.py
+++ src/fava/ext/__init__.py
@@ -12,6 +12,10 @@ from typing import TYPE_CHECKING
 from fava.helpers import BeancountError
 
 if TYPE_CHECKING:  # pragma: no cover
+    from typing import Callable
+
+    from flask.wrappers import Response
+
     from fava.beans.abc import Directive
     from fava.core import FavaLedger
 
@@ -35,6 +39,8 @@ class FavaExtensionBase:
 
     config: Any
 
+    endpoints: dict[tuple[str, str], Callable[[FavaExtensionBase], Any]]
+
     def __init__(self, ledger: FavaLedger, config: str | None = None) -> None:
         """Initialise extension.
 
@@ -43,6 +49,16 @@ class FavaExtensionBase:
             config: Configuration options string passed from the
                     beancount file's 'fava-extension' line.
         """
+        self.endpoints = {}
+
+        # Go through each of the subclass's functions to find the ones
+        # marked as endpoints by @extension_endpoint
+        for _, func in inspect.getmembers(self.__class__, inspect.isfunction):
+            if hasattr(func, "endpoint_key"):
+                name, methods = func.endpoint_key
+                for method in methods:
+                    self.endpoints[(name, method)] = func
+
         self.ledger = ledger
         try:
             self.config = ast.literal_eval(config) if config else None
@@ -58,6 +74,15 @@ class FavaExtensionBase:
     def extension_dir(self) -> Path:
         """Directory to look for templates directory and Javascript code."""
         return Path(inspect.getfile(self.__class__)).parent
+
+    def set_endpoint(
+        self,
+        endpoint_name: str,
+        func: Callable[[FavaExtensionBase], Any],
+        methods: list[str] | None = None,
+    ) -> None:
+        for method in methods or ["GET"]:
+            self.endpoints[(endpoint_name, method)] = func
 
     def after_entry_modified(self, entry: Directive, new_lines: str) -> None:
         """Run after an `entry` has been modified."""
@@ -126,3 +151,49 @@ def find_extensions(
         )
 
     return classes, []
+
+
+def extension_endpoint(
+    func_or_endpoint_name: Callable[[FavaExtensionBase], Any]
+    | str
+    | None = None,
+    methods: list[str] | None = None,
+) -> (
+    Callable[[FavaExtensionBase], Response]
+    | Callable[
+        [Callable[[FavaExtensionBase], Response]],
+        Callable[[FavaExtensionBase], Response],
+    ]
+):
+    """Decorator to mark a function as an endpoint.
+
+    Can be used as @extension_endpoint or @extension_endpoint(endpoint_name, methods)
+
+    When used as @extension_endpoint, the endpoint name is the name of the function,
+    and methods is "GET"
+
+    When used as @extension_endpoint(endpoint_name, methods), the given endpoint name
+    and methods are used, but both are optional. If endpoint_name is None, default to
+    the function name, and if methods is None, default to "GET"
+    """
+    endpoint_name = (
+        func_or_endpoint_name
+        if isinstance(func_or_endpoint_name, str)
+        else None
+    )
+
+    def decorator(
+        func: Callable[[FavaExtensionBase], Response],
+    ) -> Callable[[FavaExtensionBase], Response]:
+        f: Any = func
+        f.endpoint_key = (
+            endpoint_name if endpoint_name else func.__name__,
+            methods or ["GET"],
+        )
+        return func
+
+    return (
+        decorator(func_or_endpoint_name)
+        if callable(func_or_endpoint_name)
+        else decorator
+    )
